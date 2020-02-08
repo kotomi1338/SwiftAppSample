c09ed6ad764842ab3b7147b41aabe0394849e60e
kotomi1338 <kotomin1216@gmail.com>
2020-02-08 23:14:53 +0900

    MyOkashi作成

---
+++ MyOkashi/MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by Kotomi Mizuno on 2020/02/08.
+//  Copyright © 2020 Kotomi Mizuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
